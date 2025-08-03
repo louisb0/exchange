60214c5508a9001f602349de153c8ae04431c669
louisb0 <louisbaragwanath@outlook.com>
2025-08-03 15:56:29 +1200

    Implement naive gateway for EnterOrder and OrderAccepted

---
+++ common.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <cstdint>
+#include <netinet/in.h>
+
+namespace config {
+static constexpr uint16_t GATEWAY_PORT = 3000;
+static constexpr uint16_t ENGINE_PORT = 3001;
+static constexpr uint16_t ENGINE_MULTICAST_PORT = 3002;
+static constexpr const char *ENGINE_MULTICAST_ADDR = "239.1.1.1";
+} // namespace config
+
+namespace ouch {
+struct __attribute__((packed)) enter_order_message {
+    char message_type;
+    char order_token[14];
+    uint32_t order_book_id;
+    char side;
+    uint64_t quantity;
+    int32_t price;
+};
+
+struct __attribute__((packed)) order_accepted_message {
+    char message_type;
+    uint64_t timestamp;
+    char order_token[14];
+    uint32_t order_book_id;
+    char side;
+    uint64_t order_id;
+    uint64_t quantity;
+    int32_t price;
+};
+} // namespace ouch
